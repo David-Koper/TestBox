815022a8bb3a22340b4f463615f49cd629bbbf71
David-Koper <david.kuznetsov87@gmail.com>
2022-06-20 20:51:34 +0300

    Add TestData.java

--- src/test/java/pages/components/CalendarComponent.java
+++ src/test/java/pages/components/CalendarComponent.java
@@ -6,13 +6,14 @@ import static com.codeborne.selenide.files.DownloadActions.click;
 
 public class CalendarComponent {
     public void setDate(int day, String month, String year) {
+
         $(".react-datepicker__month-select").selectOption(month);
         $(".react-datepicker__year-select").selectOption(year);
         if (day >= 10)
             $(".react-datepicker__day--0" + day + ":not(react-datepicker__day--outside-month").click();
 
         if (day < 10)
-            $(".react-datepicker__day--0" + day + ":not(react-datepicker__day--outside-month").click();
+            $(".react-datepicker__day--00" + day + ":not(react-datepicker__day--outside-month").click();
     }
 }
 
